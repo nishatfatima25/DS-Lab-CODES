936c33170fd85c9c1d685f401f12b90db0a8fce0
nishatfatima25 <its.nishatkhan004@gmail.com>
2024-12-20 00:35:55 +0530

    Update 20-Matrixdeterminant.cpp

--- DS LAB/Array/20-Matrixdeterminant.cpp
+++ DS LAB/Array/20-Matrixdeterminant.cpp
@@ -18,21 +18,12 @@ int main()
         for(int j=0; j<n; j++){
             cin>>arr[i][j];
 
-            // D = arr[0][0]*(arr[1][1]*arr[2][2] - arr[1][2]*arr[2][1]) - arr[0][1]*(arr[1][0]*arr[2][2] - arr[2][0]*arr[1][2]) + arr[0][2]*(arr[1][0]*arr[2][1] - arr[2][0]*arr[1][2]);
+            
             D = arr[0][0]*(arr[1][1]*arr[2][2] - arr[1][2]*arr[2][1]) - arr[0][1]*(arr[1][0]*arr[2][2] - arr[2][0]*arr[1][2]) + arr[0][2]*(arr[1][0]*arr[2][1] - arr[2][0]*arr[1][1]);
         }
     }
     cout << "Determinant = " << D;
     
-    // else if(m==2 && n==2){
-    //     for(int i=0 ;i<m;i++){
-    //         for(int j=0; j<n; i++){
-    //             cin>>arr[i][j];
-
-    //             D = (arr[0][0]*arr[1][1]) - (arr[0][1]*arr[1][0]);
-    //         }
-    //     }
-    //     cout << "Determinant = " <<D;
-    // }
+    
 return 0;
 }
