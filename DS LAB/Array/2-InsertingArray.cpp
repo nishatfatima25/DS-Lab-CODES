3e4dba236ae87ef5b227ad16208ffb4011066de8
nishatfatima25 <its.nishatkhan004@gmail.com>
2024-12-20 00:35:14 +0530

    Update 2-InsertingArray.cpp

--- DS LAB/Array/2-InsertingArray.cpp
+++ DS LAB/Array/2-InsertingArray.cpp
@@ -20,16 +20,14 @@ int main()
     cout << "\nEnter element to be added and the req position" << endl;
     cin>>e >> pos;
 
-    // i= n-1 last element ke index se jo element enter hua uske index tak loop run hoga i.e pos-1 
+     
     for(int i =  n-1 ; i>=pos-1; i--){
        
-        arr[i+1] = arr[i];  // aage wale index me piche wali value assign
+        arr[i+1] = arr[i];  
         
 
     }
-    arr[pos -1] = e;  // arr inc karne ke baad finally us pos pe element insert kar denge
-    //n++;  // n ki value bhi badha denge qki element increase hua
-
+    arr[pos -1] = e;  /
     cout << "updated array" << endl;
     for(int i=0; i<=n; i++){
         cout<< arr[i] << " ";
