6385b192d7debf7a1a50d051e1657b1d840c2a09
nishatfatima25 <its.nishatkhan004@gmail.com>
2024-12-20 00:37:23 +0530

    Update 6-DuplicateNumber.cpp

--- DS LAB/Array/6-DuplicateNumber.cpp
+++ DS LAB/Array/6-DuplicateNumber.cpp
@@ -11,19 +11,6 @@ int main()
         cin>> arr[i];
     }
 
-    // int ans=0;
-
-    // //array elements ko xor karaya
-    // for(int i=0; i<n; i++){
-    //     ans = ans ^ arr[i];
-    // }
-
-    // // ab 1 se n-1 tak ka xor
-    // for(int i=1; i<n; i++){
-    //     ans = ans^i;
-    // }
-
-    // cout << "Duplicate Number =  " << ans;
 
     cout << "Duplicate Number = " ;
     for (int i=0; i<n; i++){
