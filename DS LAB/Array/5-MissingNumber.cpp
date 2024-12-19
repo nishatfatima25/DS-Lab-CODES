1885a5d8a5d285584093342ea69b52b981ce684b
nishatfatima25 <its.nishatkhan004@gmail.com>
2024-12-20 00:37:01 +0530

    Update 5-MissingNumber.cpp

--- DS LAB/Array/5-MissingNumber.cpp
+++ DS LAB/Array/5-MissingNumber.cpp
@@ -6,7 +6,7 @@ int main()
     int arr[] = {1,2,3,5,6};
     n = sizeof(arr) / sizeof(arr[0])  + 1;
 
-    // xor lenge saare element array ke 
+    
     // a^a = 0
     // a^0 = a
     int xor1=0;
@@ -15,7 +15,7 @@ int main()
     }
 
 
-    //missing array ka xor
+   
     int xor2=0;
     for(int i=0; i<n-1; i++){
         xor2 = xor2 ^ arr[i];
